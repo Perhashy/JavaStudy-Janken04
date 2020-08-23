449f7c6218073cec7f99731a66e699d188a367df
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-23 18:49:30 +0900

    ゲームカウント用意

--- GameController.java
+++ GameController.java
@@ -1,5 +1,10 @@
 package Janken04;
 
 public class GameController {
-  
+  protected static int gameCount = 1;
+  public static void game() {
+    System.out.println("================================");
+    System.out.println(gameCount + "回目のゲーム！");
+
+  }
 }
--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ package Janken04;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Mainメソッド");
+    System.out.println("じゃんけんを始めます！");
+    GameController.game();
   }
 }
