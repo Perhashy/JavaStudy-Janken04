d1f8d37843b93d06971f1b44cf26d63019231d3f
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-23 20:10:29 +0900

    あいこの場合ゲームを繰り返して、drawCountにあいこの数を代入

--- GameController.java
+++ GameController.java
@@ -2,17 +2,19 @@ package Janken04;
 
 public class GameController {
   protected static int gameCount = 1;
+  protected static int drawCount = 0;
   public static void game(Hand player, Hand computer) {
     System.out.println("================================");
     System.out.println(gameCount + "回目のゲーム！");
 
-    player.setPlayerHand();
-    computer.setComputerHand();
-
-    System.out.println("--------じゃんけんぽん！--------");
-    System.out.println("あなた：" + player.strHand());
-    System.out.println("あいて：" + computer.strHand());
-
-    Rule.draw(player.getHand(), computer.getHand());
+    do {
+      player.setPlayerHand();
+      computer.setComputerHand();
+  
+      System.out.println("--------じゃんけんぽん！--------");
+      System.out.println("あなた：" + player.strHand());
+      System.out.println("あいて：" + computer.strHand());
+    } while (Rule.draw(player.getHand(), computer.getHand()));
+    System.out.println(drawCount);
   }
 }
--- Rule.java
+++ Rule.java
@@ -4,6 +4,7 @@ public class Rule {
   public static boolean draw(int player, int computer) {
     if (player == computer) {
       System.out.println("あいこです");
+      GameController.drawCount++;
       return true;
     } else {
       return false;
